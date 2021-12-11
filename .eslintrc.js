46d9a53637445699ad8e63260078ec0411df598a
Gustavo Valverde <gustavo@iterativo.do>
2021-12-10 21:28:53 -0400

    fix: remove imaginary and use eslint-airbnb

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,26 @@
 module.exports = {
-  extends: '@imaginary-cloud/react',
-
+  extends: ["airbnb", "prettier"],
   ignorePatterns: ['**/@core/**'],
 
   parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
+    allowImportExportEverywhere: false,
+    codeFrame: false
   },
+
+  env: {
+    browser: true,
+    jest: true
+},
+
+  rules: {
+    "max-len": ["error", {"code": 100}],
+    "prefer-promise-reject-errors": ["off"],
+    "react/jsx-filename-extension": ["off"],
+    "react/prop-types": ["warn"],
+    "no-return-assign": ["off"]
+  },
+
 }
