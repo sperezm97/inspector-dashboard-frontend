9de8b33e74b58401dae65335443e9ef33e63ee50
je1999 <je99alvarez@gmail.com>
2022-01-18 17:44:34 -0400

    [ADD] url endpoints

--- src/constants/api/zammadApi.js
+++ src/constants/api/zammadApi.js
@@ -1,14 +1,18 @@
 export const zammadApi = {
   tickets: 'tickets',
+  ticketById: (id) => `tickets/${id}?expand=true`,
   allTickets: 'ticket_overviews?view=all',
   ticketsByDate: 'tickets/search?query=created_at:',
   ticketsByState: 'tickets/search?query=state_id:',
   ticketsByGroup: 'tickets/search?query=group_id:',
 
+  ticketArticles: 'ticket_articles/by_ticket/',
+  postTicketArticles: 'ticket_articles',
+
   users: 'users',
   userMe: 'users/me?expand=true',
+  userById: (id) => `users/${id}?expand=true`,
   userByCedula: 'users/search?query=cedula:',
-
   allUsers: 'users?expand=true',
 
   allRols: 'roles',
