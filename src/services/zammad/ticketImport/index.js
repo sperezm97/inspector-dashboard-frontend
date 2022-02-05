86fbc69f41400c1eed03de96089c3e67c2a1f9d4
JE <je99alvarez@gmail.com>
2022-02-04 20:19:50 -0400

    [REF] import tickets

--- src/services/zammad/ticketImport/index.js
+++ src/services/zammad/ticketImport/index.js
@@ -16,22 +16,6 @@ export const postTicketImport = async (dataCsv, objAddCsv) => {
     const lengthIncidente = arrIncidente.length - 1
 
     try {
-        // user
-        const userCedula = await getUserByCedula(dataCsv.ciudadano_id)
-        if(userCedula.data[0]) {
-            idUserCiudadano = userCedula.data[0].id
-        }else {
-            const infoCedulaCiudadano = await getInfoCedula(dataCsv.ciudadano_id)
-            const objZammad = {
-                cedula: infoCedulaCiudadano.data.payload.id,
-                firstname: infoCedulaCiudadano.data.payload.names,
-                lastname: `${infoCedulaCiudadano.data.payload.firstSurname} ${infoCedulaCiudadano.data.payload.secondSurname}`,
-                phone: dataCsv.ciudadano_telefono || '',
-            }
-            const requestUser = await postUser(objZammad)
-            idUserCiudadano = requestUser.data.id
-        }
-
         // organization and group
         const organizationData = await getOrganizationByAcronym(dataCsv.institucion)
         if(organizationData.data[0]) {
@@ -42,6 +26,24 @@ export const postTicketImport = async (dataCsv, objAddCsv) => {
                 const requestGroup = await postGroup({name: dataCsv.institucion.toUpperCase(), acronimo: dataCsv.institucion.toUpperCase()})
                 idInstitucion = requestGroup.data.id
             }
+        }
+
+        // user
+        const userCedula = await getUserByCedula(dataCsv.ciudadano_id)
+        if(userCedula.data[0]) {
+            idUserCiudadano = userCedula.data[0].id
+        }else {
+            const infoCedulaCiudadano = await getInfoCedula(dataCsv.ciudadano_id)
+            const objZammad = {
+                cedula: infoCedulaCiudadano.data.payload.id,
+                firstname: infoCedulaCiudadano.data.payload.names,
+                lastname: `${infoCedulaCiudadano.data.payload.firstSurname} ${infoCedulaCiudadano.data.payload.secondSurname}`,
+                zone: dataCsv.reporte_zona_id,
+                organization_id: idInstitucion,
+                phone: dataCsv.ciudadano_telefono || '',
+            }
+            const requestUser = await postUser(objZammad)
+            idUserCiudadano = requestUser.data.id
         }
         
         // ticket
