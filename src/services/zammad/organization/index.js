0903f40d2cc0d4639092aa22bc3e7af6fc750451
JE <je99alvarez@gmail.com>
2022-02-10 13:31:19 -0400

    [ADD] get organizations api

--- src/services/zammad/organization/index.js
+++ src/services/zammad/organization/index.js
@@ -3,4 +3,6 @@ import { zammadApi } from '../../../constants/api/zammadApi'
 
 export const postOrganization = async (data) => await zammadAxios.post(zammadApi.organizations, data)
 
+export const getOrganizations = async () => await zammadAxios.get(zammadApi.organizations)
+
 export const getOrganizationByAcronym = async (acronym) => await zammadAxios.get(zammadApi.organizationsByAcronym(acronym))
